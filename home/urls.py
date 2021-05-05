e1f66008ef18592b7594cdd23e9d1c64bf93de74
Aliya Almas <itisaliya@gmail.com>
2021-05-05 16:34:16 +0600

    static

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('medorganizations', OrganizationView.as_view(), name='all_clinic'),
     path('medorganizations/<int:pk>', OrganizationDetailView.as_view(), name='clinic'),
     path('contact_us', ContactUsView.as_view(), name='email'),
     path('medorganizations/search', SearchOrganizations.as_view())
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
